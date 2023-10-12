eba1e15ab1709504d354c022e3323e1d8c2935d8
Gedeon Jesugo GBEDONOU <beethovengedeon@gmail.com>
2023-10-12 03:12:42 +0100

    Update workflow.py

--- controller/workflow.py
+++ controller/workflow.py
@@ -12,7 +12,7 @@ with open("contants.yaml", 'r') as stream:
 def run_pipeline():
     imgs = os.listdir(CONSTANTS["inp_img_pth"])   
     for img_file in imgs:   
-        file_name = img_file.split('.')[0]
+        file_name = img_file[:-4]
         aug_file_name = file_name + "_" + CONSTANTS["transformed_file_name"]
         image = cv2.imread(os.path.join(CONSTANTS["inp_img_pth"], img_file))           
         lab_pth = os.path.join(CONSTANTS["inp_lab_pth"], file_name + '.txt')                                
